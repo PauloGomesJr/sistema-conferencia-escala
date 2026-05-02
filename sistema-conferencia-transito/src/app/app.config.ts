203062753476ce0df99c733ff26e014b2fa5dca1
Paulo Gomes <pago-jr@hotmail.com>
2026-05-02 00:24:15 -0300

    feat(i18n): configura idioma e formato de data para pt-BR

--- sistema-conferencia-transito/src/app/app.config.ts
+++ sistema-conferencia-transito/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode, LOCALE_ID } from '@angular/core'; // Adicione o LOCALE_ID aqui
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
@@ -9,11 +9,24 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 
+
+// === Importações para o formato Brasileiro ===
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+// Registra os dados do idioma português
+registerLocaleData(localePt, 'pt-BR');
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimationsAsync(),
+
+    // === NOVO: Configuração global de data e idioma ===
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
     
     // Mantém o seu aplicativo instalável e funcionando offline (PWA)
     provideServiceWorker('ngsw-worker.js', {
